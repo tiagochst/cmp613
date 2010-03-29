734975d34b8a5d8830561f6a061917a50ca378c8
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-03-29 21:17:07 +0000

    Add q2d_b q2d_c

--- lab2-q2/q2d_c/q2c.vhdl
+++ lab2-q2/q2d_c/q2c.vhdl
@@ -1,31 +1,21 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all ;
 
-ENTITY q2d IS
-	PORT (a_0,bi_0: IN STD_LOGIC_VECTOR(0 to 3);
-	      z: BUFFER STD_LOGIC_VECTOR(0 to 3);
-	      s_0, clk: IN STD_LOGIC;
-	      ovf: OUT STD_LOGIC);
-END q2d;
+ENTITY q2c IS
+	PORT (a,bi: IN STD_LOGIC_VECTOR(0 to 3);
+          z: BUFFER STD_LOGIC_VECTOR(0 to 3);
+          s: IN STD_LOGIC;
+          ovf: OUT STD_LOGIC);
+END q2c;
 
-ARCHITECTURE struct OF q2d IS
+ARCHITECTURE struct OF q2c IS
+	SIGNAL zcor: STD_LOGIC_VECTOR(0 to 3);
 	SIGNAL b, cla: STD_LOGIC_VECTOR(0 to 3);
-	SIGNAL a, bi, z_0: STD_LOGIC_VECTOR(0 to 3); --variaveis sincronas
-	SIGNAL s, ovf_0: STD_LOGIC; --variaveis sincronas
 COMPONENT somador IS
 	PORT ( c0, a0, a1 : IN STD_LOGIC ;
 		b0, c1 : OUT STD_LOGIC ) ;
 END COMPONENT somador;
 BEGIN
-	PROCESS (clk) --carrega entradas
-	BEGIN
-		if (clk'event and clk = '1') THEN 
-			a<=a_0;
-			bi<=bi_0;
-			s<=s_0;
-		END IF;
-	END PROCESS;
-	
 	compl2: PROCESS (bi, s)
 	BEGIN IF s = '0' THEN
 			b(3)<=not bi(3) xor (not bi(0) and not bi(1) and not bi(2));
@@ -43,21 +33,25 @@ BEGIN
 	cla(3)<=(a(3) and b(3)) or ((a(3) xor b(3)) and cla(2));
 	
 	s0: COMPONENT somador
-		PORT MAP ('0', a(0), b(0), z_0(0));
+		PORT MAP ('0', a(0), b(0), z(0));
 	s1: COMPONENT somador
-		PORT MAP (cla(0), a(1), b(1), z_0(1));
+		PORT MAP (cla(0), a(1), b(1), z(1));
 	s2: COMPONENT somador
-		PORT MAP (cla(1), a(2), b(2), z_0(2));
+		PORT MAP (cla(1), a(2), b(2), z(2));
 	s3: COMPONENT somador
-		PORT MAP (cla(2), a(3), b(3), z_0(3));
+		PORT MAP (cla(2), a(3), b(3), z(3));
 		
-	ovf_0<=(b(3) and a(3) and not z_0(3)) or (not b(3) and not a(3) and z_0(3));
+	ovf<=(b(3) and a(3) and not z(3)) or (not b(3) and not a(3) and z(3));
 	
-	PROCESS (clk)
-	BEGIN
-		if (clk'event and clk = '1') THEN 
-			z<=z_0;
-			ovf<=ovf_0;
+	PROCESS (z)
+	BEGIN IF z(3) = '1' THEN --aplica compl de 2 para obter o 
+			                 --valor absoluto para o display
+			zcor(3)<=not z(3) xor (not z(0) and not z(1) and not z(2));
+			zcor(2)<=not z(2) xor (not z(0) and not z(1));
+			zcor(1)<=not z(1) xor not z(0);
+			zcor(0)<=z(0);
+		ELSE
+			zcor<=z;
 		END IF;
 	END PROCESS;
 END struct;
