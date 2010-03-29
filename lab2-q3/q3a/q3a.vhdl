5d5eb54f3a28e8a15357ddedcf18a5cb84ec245d
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-03-29 17:13:30 +0000

    q3a altearada

--- lab2-q3/q3a/q3a.vhdl
+++ lab2-q3/q3a/q3a.vhdl
@@ -3,7 +3,9 @@ USE ieee.std_logic_1164.all ;
 
 ENTITY q3a IS
 	PORT (x,y: IN STD_LOGIC_VECTOR(3 downto 0);
-	      z: OUT STD_LOGIC_VECTOR(7 downto 0));
+	      z: BUFFER STD_LOGIC_VECTOR(7 downto 0);
+	      seg: OUT STD_LOGIC_VECTOR(0 to 7);
+	      seg2: OUT STD_LOGIC_VECTOR(0 to 7));
 END q3a;
 
 ARCHITECTURE struct OF q3a IS
@@ -21,6 +23,10 @@ END COMPONENT somador;
 	b: IN STD_LOGIC_VECTOR (4 downto 1);
 	y: OUT STD_LOGIC_VECTOR (8 downto 5));
 END COMPONENT;
+COMPONENT conv_7seg IS
+	PORT (x3,x2,x1,x0: IN STD_LOGIC;
+	      y6,y5,y4,y3,y2,y1,y0: OUT STD_LOGIC);
+END COMPONENT conv_7seg;
 BEGIN
 	--multiplicador para bits menos significativos
 	z(0)<=x(0) and y(0);
@@ -41,4 +47,14 @@ BEGIN
 		PORT MAP ('0', '0', x, y, hi);
 
 	z(7 downto 4)<=hi;
+
+	disp: COMPONENT conv_7seg
+		PORT MAP (z(3), z(2), z(1), z(0), seg(6), seg(5),
+		          seg(4), seg(3), seg(2), seg(1), seg(0));
+
+	disp2: COMPONENT conv_7seg
+		PORT MAP (z(7), z(6), z(5), z(4), seg2(6), seg2(5),
+		          seg2(4), seg2(3), seg2(2), seg2(1), seg2(0));
+
+
 END struct;
