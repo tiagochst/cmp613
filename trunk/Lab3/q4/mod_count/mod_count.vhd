701acee801874bd77a2a4f4db4df97c6abc1eeae
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-07 19:29:11 +0000

    q4 ok -- problema de inicialização de variáveis. Ele deveria zerar o mod_int inicialmente mas ele continua considerando diferente de 0!! O resto está funcionado.

--- trunk/Lab3/q4/mod_count/mod_count.vhd
+++ trunk/Lab3/q4/mod_count/mod_count.vhd
@@ -3,15 +3,17 @@ USE ieee.std_logic_1164.all ;
 use IEEE.numeric_std.all;
 
 Entity mod_count is
-  port(dec,ld:in STD_LOGIC;
-       clk: in STD_LOGIC;
-       valor: STD_LOGIC_VECTOR(0 to 7);
-       seg: OUT STD_LOGIC_VECTOR(0 to 6);
-      output: BUFFER STD_LOGIC_VECTOR(3 downto 0));
+  port(enable,ld:in STD_LOGIC;
+       clk_high: in STD_LOGIC;
+       valor:IN STD_LOGIC_VECTOR(0 to 7);
+       seg1: OUT STD_LOGIC_VECTOR(0 to 6);
+       seg2: OUT STD_LOGIC_VECTOR(0 to 6));
 End mod_count;
 
 architecture behave of mod_count is
---Signal clk:STD_LOGIC;
+   Signal clk:STD_LOGIC;
+   Signal output: STD_LOGIC_VECTOR(7 downto 0);
+   constant zero:integer:=0;
 --divisor de frequencias do lab1 q4
 COMPONENT q4 
 	PORT(x:IN STD_LOGIC;
@@ -28,33 +30,33 @@ COMPONENT debounce IS
 		 pb_debounced		: OUT	STD_LOGIC);
 END COMPONENT debounce;
 
---divisor de frequencias - 27MHZ em 1.6 Hz
 
 BEGIN
- -- div: COMPONENT q4
- --     PORT MAP(clk_high,clk);
- --clk<=clk_high;
-  count_proc: PROCESS(clk,dec,ld,valor)
+  --divisor de frequencias - 27MHZ em 1.6 Hz
+  div: COMPONENT q4
+      PORT MAP(clk_high,clk);
+ 
+   count_proc: PROCESS(clk,enable,ld,valor)
     Variable count_int: integer range 0 to 255:=0; --contador
-    Variable mod_int: integer range 0 to 255;   --guarda valor para loop no relógio
+    Variable mod_int: integer range 0 to 255:=0;   --guarda valor para loop no relógio
   
   Begin
     IF(ld ='1') then 
       count_int:=0;  
       mod_int:=to_integer(unsigned(valor));   
-      output <= STD_LOGIC_VECTOR(TO_UNSIGNED(count_int,4));
-    elsif (clk='1') and (clk'event) and (dec='1') then
-      if (count_int=mod_int) then 
-        count_int:=0;
-        output <= STD_LOGIC_VECTOR(TO_UNSIGNED(count_int,4));
-      else
-        count_int:= count_int +1;
-        output <= STD_LOGIC_VECTOR(TO_UNSIGNED(count_int,4));
-       end if;
-     end if;
-  end process count_proc;
+      output <= STD_LOGIC_VECTOR(TO_UNSIGNED(count_int,8));
+    elsif ((clk='1') and (clk'event) and (enable='1') and (mod_int/=zero)) then
+      count_int:= (count_int +1) MOD mod_int;
+      output <= STD_LOGIC_VECTOR(TO_UNSIGNED(count_int,8));
+    end if;
+end process count_proc;
 
-  disp: COMPONENT conv_7seg
-		PORT MAP (output(3), output(2), output(1), output(0), seg(6), seg(5),
-		          seg(4), seg(3), seg(2), seg(1), seg(0));
+ disp: COMPONENT conv_7seg
+		PORT MAP (output(3), output(2), output(1), output(0), seg1(6), seg1(5),
+		          seg1(4), seg1(3), seg1(2), seg1(1), seg1(0));
+
+  disp2: COMPONENT conv_7seg
+		PORT MAP (output(7), output(6), output(5), output(4), seg2(6), seg2(5),
+		          seg2(4), seg2(3), seg2(2), seg2(1), seg2(0));
+
 end behave;
