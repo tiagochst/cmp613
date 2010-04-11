c056685f9e42916a5e1108299c32a8985fb0e525
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-11 17:00:39 +0000

    Questão refeita devido a mal entendimento. Saida 10 leds para direta ou esquerda....mas ver se está ok na placa.Contem 10 warning. Falta usar debouncer;

--- Lab3/q6/four_bit_barrel_shifter/four_bit_barrel_shifter.vhd
+++ Lab3/q6/four_bit_barrel_shifter/four_bit_barrel_shifter.vhd
@@ -2,20 +2,11 @@ LIBRARY ieee ;
 USE ieee.std_logic_1164.all ;
 use IEEE.numeric_std.all;
 
---direction esquerda ou direita
---nshift - 0,1,2,3
---number - numero inicial
---run - realizar operação
---shift_vec - numero a ser colocado
---output - saida
-
 Entity four_bit_barrel_shifter is
   port(direction:in STD_LOGIC;
        nshift:IN STD_LOGIC_VECTOR(1 downto 0);
        number:IN STD_LOGIC_VECTOR(3 downto 0);
-       run:in STD_LOGIC;
-       shift_vec:IN STD_LOGIC_VECTOR(3 downto 0);
-       output:OUT STD_LOGIC_VECTOR(3 downto 0));
+       output:OUT STD_LOGIC_VECTOR(9 downto 0));
 End four_bit_barrel_shifter;
 
 architecture behave of four_bit_barrel_shifter is
@@ -23,45 +14,37 @@ architecture behave of four_bit_barrel_shifter is
 
 BEGIN
 
-  Pdir:PROCESS(direction)
-   BEGIN
-     IF (direction'event AND direction='1') THEN
-       save_dir<=not(save_dir);
-     END IF;  
-  end process Pdir;
-
-  Pshifter: PROCESS(run)
+  Pshifter: PROCESS(nshift,direction)
     VARIABLE shift, b: INTEGER;
 
     BEGIN
-      IF (run'event AND run='1') THEN
         shift:=to_integer(unsigned(nshift));
-      
+        output<="0000000000";
+
         --right shift
         if direction = '0' then
           if shift = 3 then
-            output <= shift_vec(3 downto 0);
+            output(3 downto 0) <= number(3 downto 0);
           elsif shift = 2 then
-            output <= shift_vec(3 downto 1) & number(3);
+            output(4 downto 1) <= number(3 downto 0);
           elsif shift = 1 then
-            output <= shift_vec(3 downto 2)  & number(3 downto 2);
+            output(5 downto 2) <= number(3 downto 0);
           elsif shift = 0 then
-            output <= shift_vec(3) & number(2 downto 0);
+            output(6 downto 3) <= number(3 downto 0);
          end if;
        
         -- left shift
         else 
           if shift = 3 then
-            output <=  shift_vec(3 downto 0);
+            output(9 downto 6) <= number(3 downto 0) ;
           elsif shift = 2 then
-            output <=  number(3) & shift_vec(3 downto 1) ;
+            output(8 downto 5) <= number(3 downto 0) ;
           elsif shift = 1 then
-            output <=  number(3 downto 2) & shift_vec(3 downto 2) ;
+            output(7 downto 4) <= number(3 downto 0) ;
           elsif shift = 0 then
-            output <=  number(2 downto 0) & shift_vec(3) ;
+            output(6 downto 3) <= number(3 downto 0) ;
          end if;
       
       END IF;
-    END IF;
   end process Pshifter;
 end behave;
