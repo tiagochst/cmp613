7398d4be8a46859385546953fd588a32fc68e04c
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-12 03:34:45 +0000

    Relatorio da Q1 e modificacao no Latch SR

--- Lab3/q1/exemplo/latch_sr.vhd
+++ Lab3/q1/exemplo/latch_sr.vhd
@@ -2,25 +2,17 @@ Library ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY latch_sr IS
-   PORT(en,reset,set: IN STD_LOGIC;
-        q, notq: OUT STD_LOGIC);
+	PORT(en,reset,set: IN STD_LOGIC;
+	     q, notq: OUT STD_LOGIC);
 END latch_sr;
 
 ARCHITECTURE rtl OF latch_sr IS
-BEGIN latch: 
-	PROCESS(en,reset,set)
 BEGIN
-    IF en = '1' THEN
-    IF reset ='1' THEN
-		q<= '0';
-		notq<=not(set);	
-	ELSE
-	    IF set ='1' THEN
-		  q<=set;
-		  notq<= reset;
+	PROCESS(en,reset,set)
+	BEGIN
+		IF (en = '1' and (set='1' or reset='1')) THEN
+			q <= set;
+			notq <= reset;
 		END IF;
-    END IF;
-    END IF;
-
-END PROCESS latch;
-END rtl;
+	END PROCESS;
+END rtl;  
