cfa8fe2acb171ff8f30c2769b3a345f5c700ce54
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-12 18:15:11 +0000

    Q6 arrumada

--- Lab3/q6/four_bit_barrel_shifter/four_bit_barrel_shifter.vhd
+++ Lab3/q6/four_bit_barrel_shifter/four_bit_barrel_shifter.vhd
@@ -1,51 +1,48 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.all ;
-use IEEE.numeric_std.all;
+USE IEEE.numeric_std.all;
 
-Entity four_bit_barrel_shifter2 is
-port(direction:in STD_LOGIC;
-nshift:IN STD_LOGIC_VECTOR(1 downto 0);
-number:IN STD_LOGIC_VECTOR(3 downto 0);
-output:OUT STD_LOGIC_VECTOR(9 downto 0));
-End four_bit_barrel_shifter2;
+ENTITY four_bit_barrel_shifter IS
+	PORT (direction : IN STD_LOGIC;
+	      nshift:IN STD_LOGIC_VECTOR(1 downto 0);
+	      number:IN STD_LOGIC_VECTOR(3 downto 0);
+	      output:OUT STD_LOGIC_VECTOR(9 downto 0));
+END four_bit_barrel_shifter;
 
-architecture behave of four_bit_barrel_shifter2 is
-SIGNAL save_dir: STD_LOGIC:='0';
-
+ARCHITECTURE behave OF four_bit_barrel_shifter IS
+	SIGNAL save_dir: STD_LOGIC:='0';
 BEGIN
 
-Pshifter: PROCESS(nshift,direction,number)
-VARIABLE shift, b: INTEGER;
+	Pshifter: PROCESS (nshift,direction,number)
+		VARIABLE shift, b: INTEGER;
+	BEGIN
+		shift:=to_integer(unsigned(nshift));
+		output<="0000000000";
 
-BEGIN
-shift:=to_integer(unsigned(nshift));
-output<="0000000000";
+		--right shift
+		IF direction = '0' THEN
+			IF shift = 3 THEN
+			output(3 downto 0) <= number(3 downto 0);
+			ELSIF shift = 2 THEN
+			output(4 downto 1) <= number(3 downto 0);
+			ELSIF shift = 1 THEN
+			output(5 downto 2) <= number(3 downto 0);
+			ELSIF shift = 0 THEN
+			output(6 downto 3) <= number(3 downto 0);
+		END IF;
 
---right shift
-if direction = '0' then
-if shift = 3 then
-output(3 downto 0) <= number(3 downto 0);
-elsif shift = 2 then
-output(4 downto 1) <= number(3 downto 0);
-elsif shift = 1 then
-output(5 downto 2) <= number(3 downto 0);
-elsif shift = 0 then
-output(6 downto 3) <= number(3 downto 0);
-end if;
-
--- left shift
-else
-if shift = 3 then
-output(9 downto 6) <= number(3 downto 0) ;
-elsif shift = 2 then
-output(8 downto 5) <= number(3 downto 0) ;
-elsif shift = 1 then
-output(7 downto 4) <= number(3 downto 0) ;
-elsif shift = 0 then
-output(6 downto 3) <= number(3 downto 0) ;
-end if;
-
-END IF;
-end process Pshifter;
-end behave;
+		-- left shift
+		ELSE
+			IF shift = 3 THEN
+			output(9 downto 6) <= number(3 downto 0) ;
+			ELSIF shift = 2 THEN
+			output(8 downto 5) <= number(3 downto 0) ;
+			ELSIF shift = 1 THEN
+			output(7 downto 4) <= number(3 downto 0) ;
+			ELSIF shift = 0 THEN
+			output(6 downto 3) <= number(3 downto 0) ;
+		END IF;
+		END IF;
+	END PROCESS Pshifter;
+END behave;
 
