fef0dd491d8f5a99c6f2d0de9d4c191a8730c0a3
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-12 20:11:51 +0000

    Q4 funcionando com clock simetrico... Decidir qual versao vai usar

--- trunk/Lab3/q4/testefreq_mod_count/div_freq.vhd
+++ trunk/Lab3/q4/testefreq_mod_count/div_freq.vhd
@@ -3,24 +3,22 @@ USE ieee.std_logic_1164.all;
 
 ENTITY div_freq IS
 	PORT (clk: IN STD_LOGIC;
-	      clk_out: OUT STD_LOGIC);
+	      clk_out: BUFFER STD_LOGIC);
 END div_freq;
 
 ARCHITECTURE struct OF div_freq  IS
-	SIGNAL cont: integer range 0 to 24000000:= 0;
+	CONSTANT DRAT: integer := 27000000;
+	SIGNAL cont: integer range 0 to DRAT:= 0;
 BEGIN
 	PROCESS (clk) BEGIN
 		IF (clk'event AND clk='1') THEN
 			cont<=cont+1;
+			IF (cont=DRAT/2) THEN
+				clk_out <= not clk_out;
+			ELSIF (cont=DRAT) THEN
+				clk_out <= not clk_out;
+				cont<=0;
+			END IF;
 		END IF;
-		
-	    IF cont=24000000 then
-	       clk_out <= '1';
-	       cont<=0;
-	    else
-	       clk_out <='0';
-	    end if;	
-	
 	END PROCESS;
-	
 END;
