f184cbc55028b82dacbe8014332d59a9ed492d27
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-12 21:29:10 +0000

    Q5 com saida F funcionando para todas as possiveis entradas. Falta gerar saidas cn4, aeqb, p, g

--- Lab3/q5/my74181.vhd
+++ Lab3/q5/my74181.vhd
@@ -3,21 +3,26 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
 ENTITY my74181 IS
-	PORT (a, b, s   : IN SIGNED(3 downto 0);
-	      m, ci     : IN STD_LOGIC;
-	      f         : OUT SIGNED(3 downto 0);
-	      p, g      : OUT STD_LOGIC;
-	      aeqb, cn4 : OUT STD_LOGIC);
+	PORT (a0, b0, s0	: IN STD_LOGIC_VECTOR(3 downto 0);
+	      m, cn 	   	: IN STD_LOGIC;
+	      f0			: OUT STD_LOGIC_VECTOR(3 downto 0);
+	      p, g      	: OUT STD_LOGIC;
+	      aeqb, cn4 	: OUT STD_LOGIC);
 END my74181;
 
 ARCHITECTURE rtl OF my74181 IS
+	SIGNAL f, a, b, s	: SIGNED(3 downto 0);
 BEGIN
-	PROCESS (a, b, s, m, ci)
+	a <= signed(a0);
+	b <= signed(b0);
+	s <= signed(s0);
+	
+	PROCESS (a, b, s, m, cn)
 		VARIABLE inc : SIGNED(3 downto 0);
 	BEGIN
-		IF (ci = '1')
-		THEN inc := "0001";
-		ELSE inc := "0000";
+		IF (cn = '1') --logica invertida
+		THEN inc := "0000";
+		ELSE inc := "0001";
 		END IF;
 		
 		mode: CASE m IS
@@ -38,13 +43,13 @@ BEGIN
 			WHEN "0110" =>
 				f <= (a - b - "0001") + inc;
 			WHEN "0111" =>
-				f <= ((a and b) - "0001") + inc;
+				f <= ((a and not(b)) - "0001") + inc;
 			WHEN "1000" =>
-				f <= (a + (a or b)) + inc;
+				f <= (a + (a and b)) + inc;
 			WHEN "1001" =>
 				f <= (a + b) + inc;
 			WHEN "1010" =>
-				f <= ((a or not(b)) + (a or b)) + inc;
+				f <= ((a or not(b)) + (a and b)) + inc;
 			WHEN "1011" =>
 				f <= ((a and b) - "0001") + inc;
 			WHEN "1100" =>
@@ -59,38 +64,40 @@ BEGIN
 		WHEN '1' =>
 			op_sel1: CASE s IS
 			WHEN "0000" =>
-				f <= not(a) + inc;
+				f <= not(a);
 			WHEN "0001" =>
-				f <= (not(a) or not(b)) + inc;
+				f <= not(a or b);
 			WHEN "0010" =>
-				f <= (not(a) and b) + inc;
+				f <= not(a) and b;
 			WHEN "0011" =>
-				f <= ("0000") + inc;
+				f <= "0000";
 			WHEN "0100" =>
-				f <= (not(a and b)) + inc;
+				f <= not(a and b);
 			WHEN "0101" =>
-				f <= (not(b)) + inc;
+				f <= not(b);
 			WHEN "0110" =>
-				f <= (a xor b) + inc;
+				f <= a xor b;
 			WHEN "0111" =>
-				f <= (a and not(b)) + inc;
+				f <= a and not(b);
 			WHEN "1000" =>
-				f <= (not(a) or b) + inc;
+				f <= not(a) or b;
 			WHEN "1001" =>
-				f <= (not(a) xor not(b)) + inc;
+				f <= a xnor b;
 			WHEN "1010" =>
-				f <= (b) + inc;
+				f <= b;
 			WHEN "1011" =>
-				f <= (a and b) + inc;
+				f <= a and b;
 			WHEN "1100" =>
-				f <= ("0001") + inc;
+				f <= "1111";
 			WHEN "1101" =>
-				f <= (a or not(b)) + inc;
+				f <= a or not(b);
 			WHEN "1110" =>
-				f <= (a or b) + inc;
+				f <= a or b;
 			WHEN "1111" =>
-				f <= (a) + inc;
+				f <= a;
 			END CASE op_sel1;
 		END CASE mode;
 	END PROCESS;
+	
+	f0 <= std_logic_vector(f);
 END rtl;
--- Lab3/q5/q5.vhd
+++ Lab3/q5/q5.vhd
@@ -5,12 +5,14 @@ USE ieee.numeric_std.all;
 ENTITY q5 IS
 	PORT (a, b, s   : IN STD_LOGIC_VECTOR(3 downto 0);
 	      m, cn     : IN STD_LOGIC;
-	      f         : OUT STD_LOGIC_VECTOR(3 downto 0);
-	      p, g      : OUT STD_LOGIC;
-	      aeqb, cn4 : OUT STD_LOGIC);
+	      test		: OUT STD_LOGIC;
+	      seg0		: OUT STD_LOGIC_VECTOR(6 downto 0));
 END q5;
 
 ARCHITECTURE struct OF q5 IS
+SIGNAL	  f1, f2    			: STD_LOGIC_VECTOR(3 downto 0);
+SIGNAL    p1, p2, g1, g2    	: STD_LOGIC;
+SIGNAL    ab1, ab2, cn4_1, cn4_2: STD_LOGIC;
 COMPONENT \74181\ IS
 	PORT (	s: IN STD_LOGIC_VECTOR (3 downto 0);
 		m, cn: IN STD_LOGIC;
@@ -21,6 +23,17 @@ COMPONENT \74181\ IS
 		aeqb: OUT STD_LOGIC;
 		cn4: OUT STD_LOGIC);
 END COMPONENT;
+COMPONENT my74181 IS
+	PORT (a0, b0, s0	: IN STD_LOGIC_VECTOR(3 downto 0);
+	      m, cn 	   	: IN STD_LOGIC;
+	      f0			: OUT STD_LOGIC_VECTOR(3 downto 0);
+	      p, g      	: OUT STD_LOGIC;
+	      aeqb, cn4 	: OUT STD_LOGIC);
+END COMPONENT my74181;
+COMPONENT conv_7seg IS
+	PORT (x: IN STD_LOGIC_VECTOR(3 downto 0);
+	      y: OUT STD_LOGIC_VECTOR(6 downto 0));
+END COMPONENT conv_7seg;
 BEGIN
 	maxplus: COMPONENT \74181\
 	PORT MAP (s		=> s,		m		=> m,
@@ -28,9 +41,21 @@ BEGIN
               a2n	=> a(2),	a1n		=> a(1),
               a0n	=> a(0),	b3n		=> b(3),
               b2n	=> b(2),	b1n		=> b(1),
-              b0n	=> b(0), 	gn		=> g,
-              pn	=> p,		f3n		=> f(3),
-              f2n	=> f(2),	f1n		=> f(1),
-              f0n	=> f(0),	aeqb	=> aeqb,
-              cn4	=> cn4);
+              b0n	=> b(0), 	gn		=> g1,
+              pn	=> p1,		f3n		=> f1(3),
+              f2n	=> f1(2),	f1n		=> f1(1),
+              f0n	=> f1(0),	aeqb	=> ab1,
+              cn4	=> cn4_1);
+	myvhd: COMPONENT my74181
+	PORT MAP (s0	=> s,		m		=> m,
+              cn	=> cn,		a0		=> a,
+              b0	=> b, 		g		=> g2,
+              p		=> p2,		f0		=> f2,
+              cn4	=> cn4_2,	aeqb	=> ab2);
+              
+    test <= '1' WHEN std_match(f1, f2)
+    ELSE '0';
+    
+    s0: COMPONENT conv_7seg
+		PORT MAP (f2,seg0);
 END struct;
