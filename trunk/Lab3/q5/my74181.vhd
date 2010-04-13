e7030d787d6e4e906ab1f948d967bc29d8ebe281
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-13 02:32:18 +0000

    Melhorias na q5, falta p, g e cn4

--- trunk/Lab3/q5/my74181.vhd
+++ trunk/Lab3/q5/my74181.vhd
@@ -11,56 +11,59 @@ ENTITY my74181 IS
 END my74181;
 
 ARCHITECTURE rtl OF my74181 IS
-	SIGNAL f, a, b, s	: SIGNED(3 downto 0);
+	SIGNAL s, fi		: SIGNED(3 downto 0);
+	SIGNAL a, b, f, inc	: SIGNED(4 downto 0);
 BEGIN
-	a <= signed(a0);
-	b <= signed(b0);
+	--os vetores de operandos e resultado
+	--tem um bit a mais para o carry
+	a(3 downto 0) <= signed(a0);
+	a(4) <= '0';
+	b(3 downto 0) <= signed(b0);
+	b(4) <= '0';
 	s <= signed(s0);
 	
 	PROCESS (a, b, s, m, cn)
-		VARIABLE inc : SIGNED(3 downto 0);
 	BEGIN
-		IF (cn = '1') --logica invertida
-		THEN inc := "0000";
-		ELSE inc := "0001";
-		END IF;
-		
 		mode: CASE m IS
 		WHEN '0' =>
 			op_sel0: CASE s IS
 			WHEN "0000" =>
-				f <= a + inc;
+				f <= a;
 			WHEN "0001" =>
-				f <= (a or b) + inc;
+				f <= (a or b);
 			WHEN "0010" =>
-				f <= (a or not(b)) + inc;
+				f <= (a or not(b));
 			WHEN "0011" =>
-				f <= (-"0001") + inc;
+				f <= (-"00001");
 			WHEN "0100" =>
-				f <= (a + (a and not(b))) + inc;
+				f <= (a + (a and not(b)));
 			WHEN "0101" =>
-				f <= ((a or b) + (a and not(b))) + inc;
+				f <= ((a or b) + (a and not(b)));
 			WHEN "0110" =>
-				f <= (a - b - "0001") + inc;
+				f <= (a - b - "00001");
 			WHEN "0111" =>
-				f <= ((a and not(b)) - "0001") + inc;
+				f <= ((a and not(b)) - "00001");
 			WHEN "1000" =>
-				f <= (a + (a and b)) + inc;
+				f <= (a + (a and b));
 			WHEN "1001" =>
-				f <= (a + b) + inc;
+				f <= (a + b);
 			WHEN "1010" =>
-				f <= ((a or not(b)) + (a and b)) + inc;
+				f <= ((a or not(b)) + (a and b));
 			WHEN "1011" =>
-				f <= ((a and b) - "0001") + inc;
+				f <= ((a and b) - "00001");
 			WHEN "1100" =>
-				f <= (a + a) + inc;
+				f <= (a + a);
 			WHEN "1101" =>
-				f <= ((a or b) + a) + inc;
+				f <= ((a or b) + a);
 			WHEN "1110" =>
-				f <= ((a or not(b)) + a) + inc;
+				f <= ((a or not(b)) + a);
 			WHEN "1111" =>
-				f <= (a - "0001") + inc;
+				f <= (a - "00001");
 			END CASE op_sel0;
+			
+			--g <= '1' WHEN f > "01111" ELSE '0';
+			--p <= '1' WHEN f >="01111" ELSE '0';
+			--f <= f + inc;
 		WHEN '1' =>
 			op_sel1: CASE s IS
 			WHEN "0000" =>
@@ -70,7 +73,7 @@ BEGIN
 			WHEN "0010" =>
 				f <= not(a) and b;
 			WHEN "0011" =>
-				f <= "0000";
+				f <= "00000";
 			WHEN "0100" =>
 				f <= not(a and b);
 			WHEN "0101" =>
@@ -88,7 +91,7 @@ BEGIN
 			WHEN "1011" =>
 				f <= a and b;
 			WHEN "1100" =>
-				f <= "1111";
+				f <= "11111";
 			WHEN "1101" =>
 				f <= a or not(b);
 			WHEN "1110" =>
@@ -99,5 +102,17 @@ BEGIN
 		END CASE mode;
 	END PROCESS;
 	
-	f0 <= std_logic_vector(f);
+	inc <= "00000" WHEN cn='1' --logica invertida no carry
+	ELSE "00001";
+	
+	--carrys lookahead prop. e ger. (nao dependem do carry)
+	p <= '1' WHEN f > "01111" ELSE '0';
+	g <= '1' WHEN f >="01111" ELSE '0';
+	
+	--inclui carry-in no f
+	fi <= (f(3 downto 0) + inc(3 downto 0));
+	
+	aeqb <= '1' WHEN fi = "1111" ELSE '0';
+	
+	f0 <= std_logic_vector(fi);
 END rtl;
