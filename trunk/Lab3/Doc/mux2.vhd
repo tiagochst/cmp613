80b2d8112223a41c3a4016164a14d3653fef8ac7
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-14 00:43:06 +0000

    Questao 3 e seu relatorio atualizadas

--- trunk/Lab3/Doc/mux2.vhd
+++ trunk/Lab3/Doc/mux2.vhd
@@ -3,12 +3,12 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
 ENTITY mux2 IS
-	PORT (x: IN STD_LOGIC_VECTOR(3 downto 0);
-	      s: IN STD_LOGIC_VECTOR(1 downto 0); 
-	      z: OUT STD_LOGIC);
+  PORT (x: IN STD_LOGIC_VECTOR(3 downto 0);
+        s: IN STD_LOGIC_VECTOR(1 downto 0); 
+        z: OUT STD_LOGIC);
 END mux2;
 
 ARCHITECTURE rtl OF mux2 IS
 BEGIN
-	z <= x(to_integer(unsigned(s)));
+  z <= x(to_integer(unsigned(s)));
 END rtl;
--- trunk/Lab3/q3/ff_jk.vhd
+++ trunk/Lab3/q3/ff_jk.vhd
@@ -1,16 +1,21 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY ff_jk IS
-	PORT (clk, j, k: IN STD_LOGIC;
+ENTITY ff_jk IS --com entrada assincronas
+                --de clear e preset
+	PORT (clk, j, k, clr, pr: IN STD_LOGIC;
 	      q, notq: BUFFER STD_LOGIC);
 END ff_jk;
 
 ARCHITECTURE rtl OF ff_jk IS
 BEGIN
-	PROCESS (clk) BEGIN
-		IF (clk'event and clk='1') THEN
-			q<=(j and not q) or (not k and q);
+	PROCESS (clk, clr, pr) BEGIN
+		IF (clr='1') THEN
+			q <= '0';
+		ELSIF (pr='1') THEN
+			q <= '1';
+		ELSIF (clk'event and clk='1') THEN
+			q <= (j and not q) or (not k and q);
 		END IF;
 	END PROCESS;
 	
