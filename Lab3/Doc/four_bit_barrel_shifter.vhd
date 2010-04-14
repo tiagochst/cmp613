0d7be6156a860c140bae71ae18e16f18ac81121a
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-14 01:26:37 +0000

    Restaurada q3

--- Lab3/Doc/four_bit_barrel_shifter.vhd
+++ Lab3/Doc/four_bit_barrel_shifter.vhd
@@ -3,46 +3,46 @@ USE ieee.std_logic_1164.all ;
 USE IEEE.numeric_std.all;
 
 ENTITY four_bit_barrel_shifter IS
-	PORT (direction : IN STD_LOGIC;
-	      nshift:IN STD_LOGIC_VECTOR(1 downto 0);
-	      number:IN STD_LOGIC_VECTOR(3 downto 0);
-	      output:OUT STD_LOGIC_VECTOR(9 downto 0));
+  PORT (direction : IN STD_LOGIC;
+        nshift:IN STD_LOGIC_VECTOR(1 downto 0);
+        number:IN STD_LOGIC_VECTOR(3 downto 0);
+        output:OUT STD_LOGIC_VECTOR(9 downto 0));
 END four_bit_barrel_shifter;
 
 ARCHITECTURE behave OF four_bit_barrel_shifter IS
-	SIGNAL save_dir: STD_LOGIC:='0';
+  SIGNAL save_dir: STD_LOGIC:='0';
 BEGIN
 
-	Pshifter: PROCESS (nshift,direction,number)
-		VARIABLE shift, b: INTEGER;
-	BEGIN
-		shift:=to_integer(unsigned(nshift));
-		output<="0000000000";
+  Pshifter: PROCESS (nshift,direction,number)
+    VARIABLE shift, b: INTEGER;
+  BEGIN
+    shift:=to_integer(unsigned(nshift));
+    output<="0000000000";
 
-		--right shift
-		IF direction = '0' THEN
-			IF shift = 3 THEN
-			output(3 downto 0) <= number(3 downto 0);
-			ELSIF shift = 2 THEN
-			output(4 downto 1) <= number(3 downto 0);
-			ELSIF shift = 1 THEN
-			output(5 downto 2) <= number(3 downto 0);
-			ELSIF shift = 0 THEN
-			output(6 downto 3) <= number(3 downto 0);
-		END IF;
+    --right shift
+    IF direction = '0' THEN
+      IF shift = 3 THEN
+      output(3 downto 0) <= number(3 downto 0);
+      ELSIF shift = 2 THEN
+      output(4 downto 1) <= number(3 downto 0);
+      ELSIF shift = 1 THEN
+      output(5 downto 2) <= number(3 downto 0);
+      ELSIF shift = 0 THEN
+      output(6 downto 3) <= number(3 downto 0);
+    END IF;
 
-		-- left shift
-		ELSE
-			IF shift = 3 THEN
-			output(9 downto 6) <= number(3 downto 0) ;
-			ELSIF shift = 2 THEN
-			output(8 downto 5) <= number(3 downto 0) ;
-			ELSIF shift = 1 THEN
-			output(7 downto 4) <= number(3 downto 0) ;
-			ELSIF shift = 0 THEN
-			output(6 downto 3) <= number(3 downto 0) ;
-		END IF;
-		END IF;
-	END PROCESS Pshifter;
+    -- left shift
+    ELSE
+      IF shift = 3 THEN
+      output(9 downto 6) <= number(3 downto 0) ;
+      ELSIF shift = 2 THEN
+      output(8 downto 5) <= number(3 downto 0) ;
+      ELSIF shift = 1 THEN
+      output(7 downto 4) <= number(3 downto 0) ;
+      ELSIF shift = 0 THEN
+      output(6 downto 3) <= number(3 downto 0) ;
+    END IF;
+    END IF;
+  END PROCESS Pshifter;
 END behave;
 
