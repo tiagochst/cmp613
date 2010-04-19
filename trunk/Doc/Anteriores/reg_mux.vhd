af85372ddfd932f7876dac3afac0154cfea82013
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-19 18:02:38 +0000

    alterando

---
+++ trunk/Doc/Anteriores/reg_mux.vhd
@@ -0,0 +1,23 @@
+LIBRARY ieee ;
+USE ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+ENTITY reg_mux IS
+  PORT (mode_tg, load: IN STD_LOGIC;
+        rclk: OUT STD_LOGIC_VECTOR(0 to 3));
+END reg_mux;
+
+ARCHITECTURE struct OF reg_mux IS
+BEGIN
+  PROCESS (mode_tg)
+    --id do registrador a ser LOAD
+    VARIABLE creg: INTEGER :=-1;
+  BEGIN
+    IF (mode_tg'event AND mode_tg='1') THEN
+      creg:=(creg+1) MOD 4;
+    END IF;
+    
+    rclk<="0000";
+    rclk(creg)<=load;
+  END PROCESS;
+END;
