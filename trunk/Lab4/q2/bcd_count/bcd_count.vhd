1022f043b7dbecdbd8ac2798921c41dd49c695d8
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-20 19:38:24 +0000

    Q2 corrigida, Q3 pronta c/ sim

--- trunk/Lab4/q2/bcd_count/bcd_count.vhd
+++ trunk/Lab4/q2/bcd_count/bcd_count.vhd
@@ -7,7 +7,7 @@ ENTITY bcd_count IS
 END bcd_count;
 
 ARCHITECTURE rtl OF bcd_count IS
-
+	SIGNAL clk1, clk2:  STD_LOGIC;
 COMPONENT \7490\
 	PORT (set9a, set9b:    in STD_LOGIC;
 	      clra, clrb  :    in STD_LOGIC;
@@ -19,10 +19,16 @@ BEGIN
 	c0: COMPONENT \7490\
 		PORT MAP ('0', '0', not rst, not rst, clk, un(0),
 		          un(3), un(2), un(1), un(0));
+		          
+	clk1 <=	'1' WHEN un="1001" ELSE '0';    
+	
 	c1: COMPONENT \7490\
-		PORT MAP ('0', '0', not rst, not rst, (un(3) and un(0)), de(0),
+		PORT MAP ('0', '0', not rst, not rst, clk1, de(0),
 		          de(3), de(2), de(1), de(0));
+		          
+	clk2 <=	'1' WHEN de="1001" ELSE '0';
+	
     c2: COMPONENT \7490\
-		PORT MAP ('0', '0', not rst, not rst, (de(3) and de(0)), ce(0),
+		PORT MAP ('0', '0', not rst, not rst, clk2, ce(0),
 		          ce(3), ce(2), ce(1), ce(0));
 END rtl;
