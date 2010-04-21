f2262910d18051c5b46df56763334ec7ff4a65f2
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-04-21 14:12:47 +0000

    q2 relatório falta simulação

---
+++ Doc/bcd_count.vhd
@@ -0,0 +1,34 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY bcd_count IS
+	PORT (clk, rst, pre: IN STD_LOGIC;
+		  un, de, ce: BUFFER STD_LOGIC_VECTOR(3 downto 0));
+END bcd_count;
+
+ARCHITECTURE rtl OF bcd_count IS
+	SIGNAL clk1, clk2:  STD_LOGIC;
+COMPONENT \7490\
+	PORT (set9a, set9b:    in STD_LOGIC;
+	      clra, clrb  :    in STD_LOGIC;
+	      clka, clkb  :    in STD_LOGIC;
+	      qd, qc, qb, qa: out STD_LOGIC);
+END COMPONENT;
+
+BEGIN
+	c0: COMPONENT \7490\
+		PORT MAP ('0', '0', not rst, not rst, clk, un(0),
+		          un(3), un(2), un(1), un(0));
+		          
+	clk1 <=	'1' WHEN un="1001" ELSE '0';    
+	
+	c1: COMPONENT \7490\
+		PORT MAP ('0', '0', not rst, not rst, clk1, de(0),
+		          de(3), de(2), de(1), de(0));
+		          
+	clk2 <=	'1' WHEN de="1001" ELSE '0';
+	
+    c2: COMPONENT \7490\
+		PORT MAP ('0', '0', not rst, not rst, clk2, ce(0),
+		          ce(3), ce(2), ce(1), ce(0));
+END rtl;
---
+++ Doc/q2.vhd
@@ -0,0 +1,50 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+ENTITY q2 IS
+	PORT (clk_base, rst, pre: IN STD_LOGIC;
+		  clk_sel: IN STD_LOGIC_VECTOR(1 downto 0);
+		  seg0, seg1, seg2: OUT STD_LOGIC_VECTOR(6 downto 0));
+END q2;
+
+ARCHITECTURE rtl OF q2 IS
+	SIGNAL clk_v: STD_LOGIC_VECTOR(3 downto 0);
+	SIGNAL clk: STD_LOGIC;
+	SIGNAL un, de, ce: STD_LOGIC_VECTOR(3 downto 0);
+
+COMPONENT bcd_count IS
+	PORT (clk, rst, pre: IN STD_LOGIC;
+		  un, de, ce: BUFFER STD_LOGIC_VECTOR(3 downto 0));
+END COMPONENT bcd_count;
+COMPONENT freq_div IS
+	PORT (clk: IN STD_LOGIC;
+	      ratio: IN INTEGER;
+	      clk_out: BUFFER STD_LOGIC);
+END COMPONENT freq_div;
+COMPONENT conv_7seg IS
+	PORT (x: IN STD_LOGIC_VECTOR(3 downto 0);
+	      y: OUT STD_LOGIC_VECTOR(6 downto 0));
+END COMPONENT conv_7seg;
+
+BEGIN
+	d0: COMPONENT freq_div
+		PORT MAP (clk_base, 27000000, clk_v(0));
+	d1: COMPONENT freq_div
+		PORT MAP (clk_base, 27000000/4, clk_v(1));
+	d2: COMPONENT freq_div
+		PORT MAP (clk_base, 27000000/12, clk_v(2));
+	clk_v(3) <= '0';
+		
+	clk <= clk_v(to_integer(unsigned(clk_sel)));
+	
+	count: COMPONENT bcd_count
+		PORT MAP (clk, rst, pre, un, de, ce);
+		
+	s0: COMPONENT conv_7seg
+		PORT MAP (un, seg0);
+	s1: COMPONENT conv_7seg
+		PORT MAP (de, seg1);
+	s2: COMPONENT conv_7seg
+		PORT MAP (ce, seg2);
+END rtl;
