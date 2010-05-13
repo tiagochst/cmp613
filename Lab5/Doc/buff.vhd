0b2415608b551dc580bab8a527cdab9512e0da5c
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-13 20:34:06 +0000

    Relatorio meu e correção na q2

--- Lab5/Doc/buff.vhd
+++ Lab5/Doc/buff.vhd
@@ -1,44 +1,47 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-         
+        
 ENTITY buff IS
-   PORT (clk, d: IN STD_LOGIC;
-         q: OUT STD_LOGIC);
+  PORT (clk, d: IN STD_LOGIC;
+        q: OUT STD_LOGIC);
 END buff;
+--Essa entidade implementa uma verificação síncrona
+--da mudança de um botão (d). A saída é '1' quando
+--o botão muda de low para high e '0' caso contrário.
 
 ARCHITECTURE rtl OF buff IS
-   TYPE state_type IS (st_wait, st_high);
-   SIGNAL state: state_type;
+  TYPE state_type IS (st_wait, st_high);
+  SIGNAL state: state_type;
 BEGIN
-   PROCESS (clk) BEGIN
-      IF (clk'event and clk = '1') THEN
-         CASE state IS
-            WHEN st_wait =>
-               IF (d = '1') THEN
-                  state <= st_high;
-               ELSE
-                  state <= st_wait;
-               END IF;
-            WHEN st_high =>
-               IF (d = '0') THEN
-                  state <= st_wait;
-               ELSE
-                  state <= st_high;
-               END IF;
-         END CASE;
-      END IF;
-   END PROCESS;
-
-   PROCESS (state, d) BEGIN
+  PROCESS (clk) BEGIN
+    IF (clk'event and clk = '1') THEN
       CASE state IS
-         WHEN st_wait =>
-            IF (d = '1') THEN
-               q <= '1';
-            ELSE
-               q <= '0';
-            END IF;
-         WHEN st_high =>
-            q <= '0';
+        WHEN st_wait =>
+          IF (d = '1') THEN
+            state <= st_high;
+          ELSE
+            state <= st_wait;
+          END IF;
+        WHEN st_high =>
+          IF (d = '0') THEN
+            state <= st_wait;
+          ELSE
+            state <= st_high;
+          END IF;
       END CASE;
-   END PROCESS;
+    END IF;
+  END PROCESS;
+
+  PROCESS (state, d) BEGIN
+    CASE state IS
+      WHEN st_wait =>
+        IF (d = '1') THEN
+          q <= '1';
+        ELSE
+          q <= '0';
+        END IF;
+      WHEN st_high =>
+        q <= '0';
+    END CASE;
+  END PROCESS;
 END rtl;
---
+++ Lab5/Doc/q4.vhd
@@ -0,0 +1,33 @@
+LIBRARY ieee;
+USE ieee.STD_LOGIC_1164.all;
+
+ENTITY q4 IS
+  PORT (data: IN STD_LOGIC_VECTOR(3 downto 0);
+        err: IN STD_LOGIC_VECTOR(6 downto 0);
+        d_cor, d_ori: OUT STD_LOGIC_VECTOR(3 downto 0));
+END q4;
+
+ARCHITECTURE behav OF q4 IS
+  SIGNAL ham, ham_err: STD_LOGIC_VECTOR(6 downto 0);
+
+COMPONENT hamming_enc IS
+  PORT (x: IN STD_LOGIC_VECTOR(3 downto 0);
+        q: OUT STD_LOGIC_VECTOR(6 downto 0));
+END COMPONENT hamming_enc;
+
+COMPONENT hamming_dec IS
+  PORT (x: IN STD_LOGIC_VECTOR(6 downto 0);
+        q: OUT STD_LOGIC_VECTOR(3 downto 0));
+END COMPONENT hamming_dec;
+
+BEGIN
+  enc: COMPONENT hamming_enc
+    PORT MAP (data, ham);
+    
+  ham_err <= ham xor err; --aplica erros desejados
+  
+  dec: COMPONENT hamming_dec
+    PORT MAP (ham_err, d_cor);
+    
+  d_ori <= data;
+END behav;
