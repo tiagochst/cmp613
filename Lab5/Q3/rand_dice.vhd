6edacfab0e92f6b23182dcf71e64ab144250a32b
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-13 02:27:27 +0000

    indentando e padronizando

--- Lab5/Q3/rand_dice.vhd
+++ Lab5/Q3/rand_dice.vhd
@@ -1,49 +1,49 @@
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.math_real.all;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+USE ieee.math_real.all;
 
-entity rand_dice is 
-port( n_ld,clk : in std_logic;
-      seg: OUT STD_LOGIC_VECTOR(6 downto 0));
-end entity rand_dice;
+ENTITY rand_dice is 
+  PORT( n_ld,clk : in std_logic;
+        seg: OUT STD_LOGIC_VECTOR(6 downto 0));
+END ENTITY rand_dice;
 
 architecture rtl of rand_dice is
-SIGNAL output,aux:STD_LOGIC_VECTOR(2 downto 0);
-SIGNAL ld:STD_LOGIC;
+  SIGNAL output,aux:STD_LOGIC_VECTOR(2 downto 0);
+  SIGNAL ld:STD_LOGIC;
 
 --display mostra valores aleatórios
 COMPONENT conv_7seg IS
-	PORT (x3,x2,x1,x0: IN STD_LOGIC;
-	      y: OUT STD_LOGIC_VECTOR(6 downto 0));
+  PORT (x3,x2,x1,x0: IN STD_LOGIC;
+        y: OUT STD_LOGIC_VECTOR(6 downto 0));
 END COMPONENT conv_7seg;
 
 --gera valores 1 a 6 freq - 27MHz
 COMPONENT cnt6 IS
-	PORT (clk: IN STD_LOGIC;
-	      num: OUT STD_LOGIC_VECTOR(2 downto 0));
+  PORT (clk: IN STD_LOGIC;
+        num: OUT STD_LOGIC_VECTOR(2 downto 0));
 END COMPONENT cnt6;
 
 COMPONENT buff IS
-	PORT (clk, d: IN STD_LOGIC;
-	      q: OUT STD_LOGIC);
+  PORT (clk, d: IN STD_LOGIC;
+        q: OUT STD_LOGIC);
 END COMPONENT buff;
 
-begin
-cnt: COMPONENT cnt6 
-	PORT MAP (clk,aux);
+BEGIN
+  cnt: COMPONENT cnt6
+        PORT MAP (clk,aux);
 
-bf: COMPONENT buff
-		PORT MAP (clk, not n_ld, ld);
+  bf: COMPONENT buff
+      	PORT MAP (clk, not n_ld, ld);
 
-process(ld,clk) begin
-	if(clk'event and clk = '1') then
-     if(ld = '1') then
-       output<=aux ;
-       end if;
-     end if;
-end process;
+  PROCESS(ld,clk) BEGIN
+	IF(clk'event and clk = '1') then
+          IF(ld = '1') then
+            output<=aux ;
+          END IF;
+        END IF;
+  END PROCESS;
 
 s0: COMPONENT conv_7seg
-	PORT MAP ('0',output(2), output(1), output(0), seg);
+       PORT MAP ('0',output(2), output(1), output(0), seg);
 END rtl;
