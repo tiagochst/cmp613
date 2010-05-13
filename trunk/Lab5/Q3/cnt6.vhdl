6b57cd32e3283b098cfc08a6a6e489bb035b934c
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-13 02:27:27 +0000

    indentando e padronizando

--- trunk/Lab5/Q3/cnt6.vhdl
+++ trunk/Lab5/Q3/cnt6.vhdl
@@ -3,28 +3,26 @@
 --------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
+USE ieee.std_logic_unsigned.all;
 
-entity cnt6 is
-port (clk : in std_logic;
+ENTITY cnt6 is
+  port (clk : in std_logic;
         num : out std_logic_vector (2 downto 0));
-end cnt6;
+END cnt6;
+
 architecture rtl of cnt6 is
-signal cont: std_logic_vector(2 downto 0):="001";
+  SIGNAL cont: std_logic_vector(2 downto 0):="001";
 
 BEGIN
-	PROCESS (clk) BEGIN
-		IF (clk='1' and clk'event) THEN
-				IF (cont + "1" > 6) THEN
-					cont <= "001";
-				ELSE
-					cont <= cont + "1";
-				END IF;
-			END IF;
-	END PROCESS;
-	
-	num <= cont;
-
-
-
-end rtl;
+   PROCESS (clk) BEGIN
+      IF (clk='1' and clk'event) THEN
+            IF (cont + "1" > 6) THEN
+               cont <= "001";
+            ELSE
+               cont <= cont + "1";
+            END IF;
+         END IF;
+   END PROCESS;
+   
+   num <= cont;
+END rtl;
