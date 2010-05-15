473a80d4c3ddf6667fd001e13710f6b308453d67
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-15 00:49:58 +0000

    Conversão pacman para uso de RAM

--- Projeto/pacman/pac_defs.vhd
+++ Projeto/pacman/pac_defs.vhd
@@ -14,8 +14,8 @@ PACKAGE pac_defs IS
 	--Maior dimensao do tabuleiro (em casas)
 	CONSTANT TAB_LEN: INTEGER := 91; 
 	
-	subtype color3 is std_logic_vector(2 downto 0);
-	type vcolor is array(0 to 3) of color3; 
+	SUBTYPE color3 is std_logic_vector(2 downto 0);
+	TYPE vcolor is array(0 to 3) of color3; 
 	
 	-- cores de cada tipo de bloco numérico tab_sym
 	CONSTANT COLORS: vcolor := ("000", "001", "111", "100"); 
@@ -24,19 +24,20 @@ PACKAGE pac_defs IS
 	--Os números representam elementos visuais na tela e o resto
 	--representa posições especiais
 	--' ': vazio, '.': caminho, 1: parede, 2: moeda, 3: porta
-	type tab_sym is (' ', '.', '1', '2', '3');         
-	type tab is array(0 to SCR_HGT-1, 0 to SCR_WDT-1) of tab_sym;
+	TYPE tab_sym is (' ', '.', '1', '2', '3');
+	TYPE tab is array(0 to SCR_HGT-1, 0 to SCR_WDT-1) of tab_sym;
+	TYPE tab_sym_3x3 is array(-1 to 1, -1 to 1) of tab_sym;
 	
-	type ovl_bitmap5 is array(0 to 4, 0 to 4) of STD_LOGIC;
-	type ovl_bitmap5_vet is array(0 to 3) of ovl_bitmap5; 
+	TYPE ovl_bitmap5 is array(0 to 4, 0 to 4) of STD_LOGIC;
+	TYPE ovl_bitmap5_vet is array(0 to 3) of ovl_bitmap5; 
 	   
 	--Fator de divisão do clock de 27MHz, usada para atualização do
 	--estado do jogo ("velocidade de execução")
 	CONSTANT DIV_FACT: INTEGER := 2700000;
 	
 	subtype sentido is INTEGER range -1 to 1;
-	type direc is array(0 to 1) of sentido;
-	type direc_vet is array(0 to 3) of direc;
+	TYPE direc is array(0 to 1) of sentido;
+	TYPE direc_vet is array(0 to 3) of direc;
 	
 	--Quatro vetores de direção: cima, direita, baixo, esquerda
 	CONSTANT DIRS: direc_vet := ((-1,0), (0,1), (1,0), (0,-1));
