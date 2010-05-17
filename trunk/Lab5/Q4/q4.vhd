78f7852a4c955a30ee3d95ceab5b162cdeb3c2d1
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-17 19:23:54 +0000

    Q4 corrigida de acordo com cortes

--- trunk/Lab5/Q4/q4.vhd
+++ trunk/Lab5/Q4/q4.vhd
@@ -4,11 +4,12 @@ USE ieee.STD_LOGIC_1164.all;
 ENTITY q4 IS
 	PORT (data: IN STD_LOGIC_VECTOR(3 downto 0);
 	      err: IN STD_LOGIC_VECTOR(6 downto 0);
-	      d_cor, d_ori: OUT STD_LOGIC_VECTOR(3 downto 0));
+	      d_cor: OUT STD_LOGIC_VECTOR(3 downto 0);
+	      ham_err: OUT STD_LOGIC_VECTOR(6 downto 0));
 END q4;
 
 ARCHITECTURE behav OF q4 IS
-	SIGNAL ham, ham_err: STD_LOGIC_VECTOR(6 downto 0);
+	SIGNAL ham: STD_LOGIC_VECTOR(6 downto 0);
 
 COMPONENT hamming_enc IS
 	PORT (x: IN STD_LOGIC_VECTOR(3 downto 0);
@@ -23,11 +24,10 @@ END COMPONENT hamming_dec;
 BEGIN
 	enc: COMPONENT hamming_enc
 		PORT MAP (data, ham);
-		
+	
+	--aplica erros definidos pelos toggle sw
 	ham_err <= ham xor err;
 	
 	dec: COMPONENT hamming_dec
-		PORT MAP (ham_err, d_cor);
-		
-	d_ori <= data;
+		PORT MAP (ham xor err, d_cor);		
 END behav;
