18774eec42246fe3fb15648bbfeb44d6de45aaf4
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-27 21:41:08 +0000

    q3a

--- Lab6/q3/q3a/ram_64B.vhd
+++ Lab6/q3/q3a/ram_64B.vhd
@@ -23,35 +23,32 @@ ARCHITECTURE behav OF ram_64B IS
 	SIGNAL wren_sig,rden_sig: STD_LOGIC;
 	SIGNAL q_sig : STD_LOGIC_VECTOR (7 DOWNTO 0);
 
-	COMPONENT cpm_ram IS
-		PORT
-		(
-			clock		: IN STD_LOGIC  := '1';
-			data		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-			rdaddress		: IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-			rden		: IN STD_LOGIC  := '0';
-			wraddress		: IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-			wren		: IN STD_LOGIC  := '0';
-			q		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
-		);
-	END COMPONENT cpm_ram;
+COMPONENT ram1p IS
+	PORT
+	(
+		clock		: IN STD_LOGIC  := '1';
+		data		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		rdaddress		: IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+		rden		: IN STD_LOGIC  := '1';
+		wraddress		: IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+		wren		: IN STD_LOGIC  := '0';
+		q		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+	);
+
+END COMPONENT ram1p;
 
 
 BEGIN
 --write
-PROCESS(clk,wren,rden) BEGIN
-	IF(clk'event and clk = '1') then
+PROCESS(clk,wren,rden,chipen)
+ BEGIN
 		IF(chipen = '1') THEN
 			IF(wren='1' and rden ='0') THEN
-				clock_sig<='1';
 				wren_sig <= '1';
 				rden_sig <= '0';
-				data_sig <= input;
-				address_sig <= address;
 			ELSIF(wren='0' and rden ='1') THEN
 				wren_sig <= '0';
 				rden_sig <= '1';
-				address_sig <= address;
 			ELSE
 				wren_sig <= '0';
 				rden_sig <= '0';
@@ -60,15 +57,19 @@ PROCESS(clk,wren,rden) BEGIN
 			wren_sig <= '0';
 			rden_sig <= '0';
 		END IF;
-	END IF;	
 END PROCESS;
---PROCESS(q_sig) BEGIN
---output<=q_sig;
---END PROCESS;
 
-	cpm_ram_inst : cpm_ram PORT MAP (
-		clk,data_sig,address_sig,rden_sig,address_sig,wren_sig,
-		output
+PROCESS(q_sig,chipen) BEGIN
+		IF(chipen = '1') THEN
+			output<=q_sig;
+		ELSE
+			output<= (others=>'Z');
+		END IF;
+END PROCESS;
+
+
+ram1p_inst : ram1p PORT MAP (
+		clk,input,address,rden_sig,address,wren_sig,q_sig
 	);
 
 END behav;
