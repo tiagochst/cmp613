7d7d9243f1fd4650c6328e84fcaae410d5538f7a
tiagochst <tiagochst@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-27 02:26:06 +0000

    q3 com varios problemas

--- Lab6/q3/q3a/ram_64B.vhd
+++ Lab6/q3/q3a/ram_64B.vhd
@@ -5,12 +5,13 @@ use ieee.numeric_std.all;
 ENTITY ram_64B IS
 	PORT
 	(
-		input     : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		output    : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-		address   : IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-		wren      : IN STD_LOGIC  := '0';
-		chipen    : IN STD_LOGIC  := '0';
-		outen    : OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+		clk:IN STD_LOGIC  := '0'; 
+		input     : IN STD_LOGIC_VECTOR (7 DOWNTO 0);  -- dados entrada
+		output    : OUT STD_LOGIC_VECTOR (7 DOWNTO 0); -- saida
+		address   : IN STD_LOGIC_VECTOR (5 DOWNTO 0);  --endereco
+		wren      : IN STD_LOGIC;                       -- W
+		chipen    : IN STD_LOGIC;                       --E
+		outen    : IN STD_LOGIC                        --G
 	);
 END ram_64B;
 
@@ -20,19 +21,14 @@ ARCHITECTURE behav OF ram_64B IS
 	SIGNAL clken_sig : STD_LOGIC ;
 	SIGNAL data_sig: STD_LOGIC_VECTOR (7 DOWNTO 0);
 	SIGNAL wren_sig: STD_LOGIC;
-	SIGNAL q_sig : STD_LOGIC_VECTOR (7 DOWNTO 0);
-
-   COMPONENT conv_7seg IS
-	  PORT (x: IN STD_LOGIC_VECTOR(3 downto 0);
-	        y: OUT STD_LOGIC_VECTOR(6 downto 0));
-    END COMPONENT conv_7seg;
+	SIGNAL output_sig : STD_LOGIC_VECTOR (7 DOWNTO 0);
 
 	COMPONENT newram IS
 		PORT
 		(
 		address		: IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-		clken		: IN STD_LOGIC  := '1';
-		clock		: IN STD_LOGIC  := '1';
+		clken		: IN STD_LOGIC  ;
+		clock		: IN STD_LOGIC  ;
 		data		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
 		wren		: IN STD_LOGIC ;
 		q		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
@@ -40,15 +36,37 @@ ARCHITECTURE behav OF ram_64B IS
 	END COMPONENT newram;
 
 BEGIN
-		clken_sig <= chipen;
-		
-		newram_inst : newram PORT MAP (
-		address	 => address_sig,
-		clken	 => clken_sig,
-		clock	 => clock_sig,
-		data	 => data_sig,
-		wren	 => wren_sig,
-		q	 => q_sig
+--write
+PROCESS(clk) BEGIN
+	IF(clk'event and clk = '1') then
+		IF(chipen = '1') THEN
+			clken_sig<='1';
+			IF(wren='1') THEN
+				wren_sig <= '1';
+				data_sig <= input;
+				address_sig <= address;
+			ELSE
+				wren_sig<='0';
+			END IF;
+			IF(outen='1') THEN
+				wren_sig <= '0';
+				address_sig <= address;
+				output<=output_sig;
+			ELSE
+				wren_sig<='0';
+			END IF;
+			ELSE clken_sig<='0';
+		END IF;
+	END IF;	
+END PROCESS;
+
+newram_inst : newram PORT MAP (
+		address	 => address,--address_sig,
+		clken	 => '1',--clken_sig,
+		clock	 => clk,--clock_sig,
+		data	 => input,--data_sig,
+		wren	 => wren,--wren_sig,
+		q	 => output
 	);
 
 END behav;
