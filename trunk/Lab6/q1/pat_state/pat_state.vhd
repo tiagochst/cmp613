8d094b8ce8778bcaa766dbed605515dd2882a6e2
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-28 20:02:34 +0000

    Questoes 1 e 2 com detector de estados corrigido

--- trunk/Lab6/q1/pat_state/pat_state.vhd
+++ trunk/Lab6/q1/pat_state/pat_state.vhd
@@ -3,21 +3,25 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 	      
 ENTITY pat_state IS
-	GENERIC (
-		PATT: STD_LOGIC_VECTOR(0 to 7) := "10110001"
-	);
 	PORT (clk, rstn: IN STD_LOGIC;
 	      pb0, pb1: IN STD_LOGIC;
 	      cur_st: OUT STD_LOGIC_VECTOR(2 downto 0);
 	      ok: OUT STD_LOGIC);
 END pat_state;
 
-ARCHITECTURE behav OF pat_state IS
+ARCHITECTURE behav OF pat_state IS 
 	CONSTANT PAT_LEN: INTEGER := 8;
+	SUBTYPE index is INTEGER range 0 to PAT_LEN-1;
+	TYPE int_array is ARRAY(0 to PAT_LEN-1) OF index;
+	CONSTANT PATT: --padrão desejado de busca
+	   STD_LOGIC_VECTOR(0 to PAT_LEN-1) := "10110001";
+	CONSTANT FAIL: --próximo estado se a busca falhar em i
+	               --deve ser coerente com o padrão PATT
+	   int_array := (0, 1, 0, 2, 1, 3, 1, 0);
+
 	SIGNAL zero, um: STD_LOGIC; --sinais síncronos
-	
 	SIGNAL st_no, nxt_st: INTEGER range 0 to PAT_LEN:=0;
-	SIGNAL look_pos: INTEGER range 0 to PAT_LEN-1;
+	SIGNAL look_pos: index;
 	--estados 0 e PAT_LEN correspondem ao primeiro bit,
     --mas no último a sequência acabou de ser encontrada
 	--look_pos é o índice real do próximo bit esperado
@@ -43,7 +47,7 @@ BEGIN
 			ELSIF (um = '1' and PATT(look_pos) = '1') THEN
 				st_no <= nxt_st;
 			ELSIF (zero = '1' or um = '1') THEN
-				st_no <= 0;
+				st_no <= FAIL(look_pos);
 			END IF;
 		END IF;
 	END PROCESS;
