810ff790906db19bf3236e2cc88fc31c4cc55855
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-28 03:09:01 +0000

    Simulações e correções nas questões 1 e 2

--- trunk/Lab6/q2/word_detect.vhd
+++ trunk/Lab6/q2/word_detect.vhd
@@ -20,11 +20,10 @@ ARCHITECTURE behav OF word_detect IS
     CONSTANT WORD: hword := (x"C",x"0",x"F",x"F",x"E",x"E");
     
 	SIGNAL sn_nxt: STD_LOGIC; --sinal síncrono
-	SIGNAL last_x: hexa := x"0";
+	SIGNAL last_x: hexa := x"0"; --último número entrado
 	
-	SIGNAL state_no, nxt_state: INTEGER range 0 to W_LEN := 0;
-	--estados 0 e WLEN correspondem ao primeiro bit, mas
-	--em WLEN a sequência acabou de ser encontrada
+	SIGNAL st_no, nxt_st: INTEGER range 0 to W_LEN := 0;
+	SIGNAL look_pos: INTEGER range 0 to W_LEN-1;
 	
 	COMPONENT buff IS
     PORT (clk, d: IN STD_LOGIC;
@@ -39,17 +38,17 @@ BEGIN
 	bf0: COMPONENT buff
 		PORT MAP (clk, not nxt, sn_nxt);
 
-	PROCESS (clk, rstn, state_no)
+	PROCESS (clk, rstn, st_no)
 	BEGIN
 		IF (rstn = '0') THEN
-			state_no <= 0;
+			st_no <= 0;
 		ELSIF (rising_edge(clk)) THEN
 			--Transição da Máquina de Estados
             IF (sn_nxt = '1') THEN
-                IF (x = WORD(state_no)) THEN
-                    state_no <= nxt_state;
-                ELSE 
-                    state_no <= 0;
+                IF (x = WORD(look_pos)) THEN
+                    st_no <= nxt_st;
+                ELSE
+                    st_no <= 0;
                 END IF;
                 last_x <= x;
             END IF;
@@ -58,20 +57,22 @@ BEGIN
 	
 	--Saídas e sinais de controle da Máquina de Estados
 	--Calcula próximo estado se a entrada vier correta
-	PROCESS (state_no)
+	PROCESS (st_no)
 	BEGIN
-		CASE state_no IS
+		CASE st_no IS
 			WHEN 0 to W_LEN-1 =>
 				ok <= '0';
-				nxt_state <= state_no + 1;
+				nxt_st <= st_no + 1;
+				look_pos <= st_no;
 			WHEN W_LEN =>
 				ok <= '1';
-				nxt_state <= 1;
+				nxt_st <= 1;
+				look_pos <= 0;
 		END CASE;
 	END PROCESS;
     
     s0: COMPONENT conv_7seg
         PORT MAP (last_x, seg0);
 	
-	cur_st <= std_logic_vector(to_unsigned(state_no, 3));
+	cur_st <= std_logic_vector(to_unsigned(st_no, 3));
 END behav;
