1493dd3ac281155b44c7b179348bc50ef6dd6d17
marspeoplester <marspeoplester@e1f1e482-f715-789d-06cc-1c883741dcb4>
2010-05-28 20:02:34 +0000

    Questoes 1 e 2 com detector de estados corrigido

--- Lab6/q1/pat_shift/pat_shift.vhd
+++ Lab6/q1/pat_shift/pat_shift.vhd
@@ -3,54 +3,34 @@ USE ieee.std_logic_1164.all;
 	      
 ENTITY pat_shift IS
 	GENERIC (
-		PATT: STD_LOGIC_VECTOR(0 to 7) := "10110001"
+		PAT_LEN: INTEGER := 8
 	);
 	PORT (clk, rstn: IN STD_LOGIC;
 	      pb0, pb1: IN STD_LOGIC;
-	      shreg: OUT STD_LOGIC_VECTOR(7 downto 0);
+	      shreg: OUT STD_LOGIC_VECTOR(PAT_LEN-1 downto 0);
 	      ok: OUT STD_LOGIC);
 END pat_shift;
 
 ARCHITECTURE behav OF pat_shift IS
+	CONSTANT PATT: 
+		STD_LOGIC_VECTOR(0 to PAT_LEN-1):="10110001";
 	SIGNAL zero, um: STD_LOGIC; --sinais síncronos
 	
-	SIGNAL qb: INTEGER range 0 to 8 := 0;
-	SIGNAL x: STD_LOGIC_VECTOR(0 to 7) := "00000000";
-	--dados lidos são deslocados em x
-	
-	COMPONENT buff IS
-		PORT (clk, d: IN STD_LOGIC;
-			  q: OUT STD_LOGIC);
-	END COMPONENT buff;
+	SIGNAL det_in, det_en: STD_LOGIC;
 BEGIN
-	bf0: COMPONENT buff
+	bf0: ENTITY WORK.buff
 		PORT MAP (clk, not pb0, zero);
-	bf1: COMPONENT buff
+	bf1: ENTITY WORK.buff
 		PORT MAP (clk, not pb1, um);
-		
-	PROCESS (clk, rstn)
-	BEGIN
-		IF (rstn = '0') THEN
-			qb <= 0;
-			x  <= "00000000";
-		ELSIF (rising_edge(clk)) THEN
-			--desloca os bits caso uma entrada esteja ativa
-			IF (zero = '1') THEN
-				x(0 to 6) <= x(1 to 7);
-				x(7) <= '0';
-			ELSIF (um = '1') THEN
-				x(0 to 6) <= x(1 to 7);
-				x(7) <= '1';
-			END IF;
-			
-			IF ((zero = '1' or um = '1') and (qb < 8)) THEN
-				qb <= qb + 1; --incr número de bits lidos
-			END IF;
-		END IF;
-	END PROCESS;
 	
-	shreg <= x;
-		
-	ok <= '1' WHEN (x = PATT and qb = 8)
+	--Converte sinais para entrada binária
+	det_in <= '1' WHEN um = '1'
 	ELSE '0';
+	
+	det_en <= '1' WHEN (zero = '1' or um = '1')
+	ELSE '0';
+		
+	det0: ENTITY WORK.pat_shift_det
+		PORT MAP (clk, rstn, det_en, PATT,
+		          det_in, shreg, ok);
 END behav;
--- Lab6/q2/word_detect.vhd
+++ Lab6/q2/word_detect.vhd
@@ -6,7 +6,6 @@ ENTITY word_detect IS
 	PORT (clk, rstn: IN STD_LOGIC;
 	      x: IN STD_LOGIC_VECTOR(3 downto 0);
           nxt: IN STD_LOGIC;
-	      cur_st: OUT STD_LOGIC_VECTOR(2 downto 0);
           seg0: OUT STD_LOGIC_VECTOR(6 downto 0);
 	      ok: OUT STD_LOGIC);
 END word_detect;
@@ -14,65 +13,56 @@ END word_detect;
 ARCHITECTURE behav OF word_detect IS
     CONSTANT W_LEN: NATURAL := 6;
 
-    SUBTYPE hexa IS STD_LOGIC_VECTOR(3 downto 0);
-    TYPE hword IS ARRAY(0 to W_LEN-1) OF hexa;
-    
-    CONSTANT WORD: hword := (x"C",x"0",x"F",x"F",x"E",x"E");
+	SUBTYPE hexa IS STD_LOGIC_VECTOR(3 downto 0);
+    SUBTYPE stream IS 
+       STD_LOGIC_VECTOR(W_LEN-1 downto 0);
+    TYPE hword IS ARRAY(3 downto 0) OF stream;
+                             --COFFEE--
+    CONSTANT WORDS: hword := ("101111",
+                              "101111",
+                              "001111",
+                              "001100");
     
 	SIGNAL sn_nxt: STD_LOGIC; --sinal síncrono
 	SIGNAL last_x: hexa := x"0"; --último número entrado
 	
-	SIGNAL st_no, nxt_st: INTEGER range 0 to W_LEN := 0;
-	SIGNAL look_pos: INTEGER range 0 to W_LEN-1;
-	
-	COMPONENT buff IS
-    PORT (clk, d: IN STD_LOGIC;
-          q: OUT STD_LOGIC);
-	END COMPONENT buff;
+	SIGNAL det_oks: STD_LOGIC_VECTOR(3 downto 0);
     
     COMPONENT conv_7seg IS
 	PORT (x: IN STD_LOGIC_VECTOR(3 downto 0);
 	      y: OUT STD_LOGIC_VECTOR(6 downto 0));
     END COMPONENT conv_7seg;
 BEGIN
-	bf0: COMPONENT buff
+	bf0: ENTITY WORK.buff
 		PORT MAP (clk, not nxt, sn_nxt);
+		
+	pdet0: ENTITY WORK.pat_shift_det GENERIC MAP (6)
+		PORT MAP (clk, rstn, sn_nxt, WORDS(0),
+	              x(0), ok => det_oks(0));
+	pdet1: ENTITY WORK.pat_shift_det GENERIC MAP (6)
+		PORT MAP (clk, rstn, sn_nxt, WORDS(1),
+	              x(1), ok => det_oks(1));
+	pdet2: ENTITY WORK.pat_shift_det GENERIC MAP (6)
+		PORT MAP (clk, rstn, sn_nxt, WORDS(2),
+	              x(2), ok => det_oks(2));	                  
+	pdet3: ENTITY WORK.pat_shift_det GENERIC MAP (6)
+		PORT MAP (clk, rstn, sn_nxt, WORDS(3),
+	              x(3), ok => det_oks(3));	
 
-	PROCESS (clk, rstn, st_no)
+	PROCESS (clk, rstn)
 	BEGIN
 		IF (rstn = '0') THEN
-			st_no <= 0;
+			last_x <= x"0";
 		ELSIF (rising_edge(clk)) THEN
-			--Transição da Máquina de Estados
             IF (sn_nxt = '1') THEN
-                IF (x = WORD(look_pos)) THEN
-                    st_no <= nxt_st;
-                ELSE
-                    st_no <= 0;
-                END IF;
-                last_x <= x;
+				last_x <= x;
             END IF;
 		END IF;
 	END PROCESS;
 	
-	--Saídas e sinais de controle da Máquina de Estados
-	--Calcula próximo estado se a entrada vier correta
-	PROCESS (st_no)
-	BEGIN
-		CASE st_no IS
-			WHEN 0 to W_LEN-1 =>
-				ok <= '0';
-				nxt_st <= st_no + 1;
-				look_pos <= st_no;
-			WHEN W_LEN =>
-				ok <= '1';
-				nxt_st <= 1;
-				look_pos <= 0;
-		END CASE;
-	END PROCESS;
-    
     s0: COMPONENT conv_7seg
         PORT MAP (last_x, seg0);
-	
-	cur_st <= std_logic_vector(to_unsigned(st_no, 3));
+        
+    ok <= '1' WHEN (det_oks = "1111")
+    ELSE '0';
 END behav;
